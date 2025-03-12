a15cbcb2365f19d114e3660efae5e6553a03b825
MarioToast <70570052+MarioToast@users.noreply.github.com>
2025-03-12 19:44:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function test() {
   const inputText = document.getElementById("inputs").innerHTML;
   //const testText = document.getElementById("output").value;
   //alert(testText);
-  document.getElementById("output").innerHTML = inputText;
+  document.getElementById("output").value = inputText;
 }
