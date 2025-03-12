4a0c26d44591adff0693cc90457b3d94c88f53c7
MarioToast <70570052+MarioToast@users.noreply.github.com>
2025-03-12 20:22:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function alphabetize() {
   const inputText = document.getElementById("inputs").value;
-  const itemList = inputText.toString().split(/(\n)/);
+  const itemList = inputText.toString().split("b");
   itemList.sort();
   //alert(itemList[0]);
   document.getElementById("output").textContent = itemList;
